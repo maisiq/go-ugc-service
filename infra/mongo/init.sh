250adb6751a3e527d3b5bebc1b81e05044df6289
maisiq <114932304+maisiq@users.noreply.github.com>
2025-09-25 15:26:54 +0400

    fix(infra/mongo): mongo process correctly receives signals from docker (#2)
    Init script now listens signals from OS

--- infra/mongo/init.sh
+++ infra/mongo/init.sh
@@ -4,6 +4,17 @@ set -e
 
 mongod --replSet rs0 --bind_ip_all &
 
+PID=$!
+
+signal_handler() {
+    echo "Gracefully shutdown"
+    kill -TERM $PID
+    wait $PID
+    exit 0
+}
+
+trap signal_handler SIGINT SIGTERM
+
 until mongosh > /dev/null; do
     echo "❗ Mongosh не отвечает. Повторная проверка через 5 секунд..."
     sleep 5
@@ -12,4 +23,4 @@ done
 echo "rs.initiate()" | mongosh
 echo "🟢 Replica set is ready..."
 
-wait -n
+wait $PID
